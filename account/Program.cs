8ed826dc7d02957ba5e237699514ad0117d0f545
GennadiySerikov <serikov.gen@yandex.ru>
2021-11-18 19:35:55 +0300

    feat: account v1.0

--- account/Program.cs
+++ account/Program.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using account.Domain;
+using System;
 
 namespace account
 {
     class Program
     {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+        static void Main() {
+            TestBankAccount();
         }
+
+        static void TestBankAccount() {
+            var account = BankAccount
+                               .Create()
+                               .SetType(AccountType.Social)
+                               .SetBalance(1000m);
+
+            Console.WriteLine(account.Show());
+
+            account.TopUp(200);
+            account.TopUp(300);
+
+            Console.WriteLine(account.Show());
+
+            account.TopUp(0);
+            account.TopUp(-1);
+
+            account.WithDraw(500);
+            account.WithDraw(500);
+
+            Console.WriteLine(account.Show());
+
+            account.WithDraw(500);
+
+            Console.WriteLine(account.Show());
+        }   
     }
 }
